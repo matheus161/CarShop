52837b07fcc2332f3dddfe1f8d42119e83e1a7b6
Your Name <you@example.com>
2020-06-22 15:01:55 -0300

    Added the CORS in the app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes)
+
+app.use(cors());
 
 module.exports = app;
