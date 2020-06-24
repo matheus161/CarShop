96840e551169a2db2def92e2cd73220595600517
Your Name <you@example.com>
2020-06-24 16:48:56 -0300

    FEAT: Add .env to the project and configurating the server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const app = require("./src/app.js");
 const mongoose = require("mongoose");
 const env = require("./env.js")
+require("dotenv/config");
+
 
 const start = async () => {
-    await mongoose.connect(env.db.mongoURI, env.db.options);
-    app.listen(3001, (err) => {
+    await mongoose.connect(process.env.MONGOURI, 
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      });
+    app.listen(process.env.PORT, (err) => {
       if (err) {
         return console.log("err: ", err);
       }
-      return console.log(`Server running at http://localhost:${env.app.port}`);
+      return console.log(`Server running at http://localhost:${process.env.PORT}`);
     });
   };
+
   
   start();
   
